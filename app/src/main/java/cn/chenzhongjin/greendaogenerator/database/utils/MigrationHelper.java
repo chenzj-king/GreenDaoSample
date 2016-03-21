209690bbe53a763b911a2d9d15b7770b29baecea
chenzj <admin@chenzhongjin.cn>
2016-03-21 14:44:50 +0800

    first commit

---
+++ app/src/main/java/cn/chenzhongjin/greendaogenerator/database/utils/MigrationHelper.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2015  DreamLiner Studio
+ * Licensed under the Apache License, Version 2.0 (the "License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.chenzhongjin.greendaogenerator.database.utils;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cn.chenzhongjin.greendaogenerator.database.DaoMaster;
+import de.greenrobot.dao.AbstractDao;
+import de.greenrobot.dao.internal.DaoConfig;
+
+/**
+ * @author chenzj
+ * @Title: MigrationHelper
+ * @Description:
+ * @date
+ * @email admin@chenzhongjin.cn
+ */
+public class MigrationHelper {
+
+    private static final String CONVERSION_CLASS_NOT_FOUND_EXCEPTION = "MIGRATION HELPER - CLASS DOESN'T MATCH WITH THE CURRENT PARAMETERS";
+    private static MigrationHelper instance;
+
+    public static MigrationHelper getInstance() {
+        if (instance == null) {
+            instance = new MigrationHelper();
+        }
+        return instance;
+    }
+
+    public void migrate(SQLiteDatabase db, Class<? extends AbstractDao<?, ?>>... daoClasses) {
+        generateTempTables(db, daoClasses);
+        DaoMaster.dropAllTables(db, true);
+        DaoMaster.createAllTables(db, false);
+        restoreData(db, daoClasses);
+    }
+
+    private void generateTempTables(SQLiteDatabase db, Class<? extends AbstractDao<?, ?>>... daoClasses) {
+        for (int i = 0; i < daoClasses.length; i++) {
+            DaoConfig daoConfig = new DaoConfig(db, daoClasses[i]);
+
+            String divider = "";
+            String tableName = daoConfig.tablename;
+            String tempTableName = daoConfig.tablename.concat("_TEMP");
+            ArrayList<String> properties = new ArrayList<>();
+
+            StringBuilder createTableStringBuilder = new StringBuilder();
+
+            createTableStringBuilder.append("CREATE TABLE ").append(tempTableName).append(" (");
+
+            for (int j = 0; j < daoConfig.properties.length; j++) {
+                String columnName = daoConfig.properties[j].columnName;
+
+                if (getColumns(db, tableName).contains(columnName)) {
+                    properties.add(columnName);
+
+                    String type = null;
+
+                    try {
+                        type = getTypeByClass(daoConfig.properties[j].type);
+                    } catch (Exception exception) {
+                        System.out.print(exception.getMessage());
+                    }
+
+                    createTableStringBuilder.append(divider).append(columnName).append(" ").append(type);
+
+                    if (daoConfig.properties[j].primaryKey) {
+                        createTableStringBuilder.append(" PRIMARY KEY");
+                    }
+
+                    divider = ",";
+                }
+            }
+            createTableStringBuilder.append(");");
+
+            db.execSQL(createTableStringBuilder.toString());
+
+            StringBuilder insertTableStringBuilder = new StringBuilder();
+
+            insertTableStringBuilder.append("INSERT INTO ").append(tempTableName).append(" (");
+            insertTableStringBuilder.append(TextUtils.join(",", properties));
+            insertTableStringBuilder.append(") SELECT ");
+            insertTableStringBuilder.append(TextUtils.join(",", properties));
+            insertTableStringBuilder.append(" FROM ").append(tableName).append(";");
+
+            db.execSQL(insertTableStringBuilder.toString());
+        }
+    }
+
+    private void restoreData(SQLiteDatabase db, Class<? extends AbstractDao<?, ?>>... daoClasses) {
+        for (int i = 0; i < daoClasses.length; i++) {
+            DaoConfig daoConfig = new DaoConfig(db, daoClasses[i]);
+
+            String tableName = daoConfig.tablename;
+            String tempTableName = daoConfig.tablename.concat("_TEMP");
+            ArrayList<String> properties = new ArrayList();
+
+            for (int j = 0; j < daoConfig.properties.length; j++) {
+                String columnName = daoConfig.properties[j].columnName;
+
+                if (getColumns(db, tempTableName).contains(columnName)) {
+                    properties.add(columnName);
+                }
+            }
+
+            StringBuilder insertTableStringBuilder = new StringBuilder();
+
+            insertTableStringBuilder.append("INSERT INTO ").append(tableName).append(" (");
+            insertTableStringBuilder.append(TextUtils.join(",", properties));
+            insertTableStringBuilder.append(") SELECT ");
+            insertTableStringBuilder.append(TextUtils.join(",", properties));
+            insertTableStringBuilder.append(" FROM ").append(tempTableName).append(";");
+
+            StringBuilder dropTableStringBuilder = new StringBuilder();
+
+            dropTableStringBuilder.append("DROP TABLE ").append(tempTableName);
+
+            db.execSQL(insertTableStringBuilder.toString());
+            db.execSQL(dropTableStringBuilder.toString());
+        }
+    }
+
+    private String getTypeByClass(Class<?> type) throws Exception {
+        if (type.equals(String.class)) {
+            return "TEXT";
+        }
+        if (type.equals(Long.class) || type.equals(Integer.class) || type.equals(long.class)) {
+            return "INTEGER";
+        }
+        if (type.equals(Boolean.class)) {
+            return "BOOLEAN";
+        }
+
+        Exception exception = new Exception(CONVERSION_CLASS_NOT_FOUND_EXCEPTION.concat(" - Class: ").concat(type.toString()));
+        System.out.print(exception.getMessage());
+        throw exception;
+    }
+
+    private static List<String> getColumns(SQLiteDatabase db, String tableName) {
+        List<String> columns = new ArrayList<>();
+        Cursor cursor = null;
+        try {
+            cursor = db.rawQuery("SELECT * FROM " + tableName + " limit 1", null);
+            if (cursor != null) {
+                columns = new ArrayList<>(Arrays.asList(cursor.getColumnNames()));
+            }
+        } catch (Exception e) {
+            Log.v(tableName, e.getMessage(), e);
+            e.printStackTrace();
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return columns;
+    }
+}
---
+++ app/src/main/java/cn/chenzhongjin/greendaogenerator/database/utils/UpgradeHelper.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2015  DreamLiner Studio
+ * Licensed under the Apache License, Version 2.0 (the "License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.chenzhongjin.greendaogenerator.database.utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import cn.chenzhongjin.greendaogenerator.database.DaoMaster;
+import cn.chenzhongjin.greendaogenerator.database.UserDao;
+
+/**
+ * @author chenzj
+ * @Title: UpgradeHelper
+ * @Description:
+ * @date
+ * @email admin@chenzhongjin.cn
+ */
+public class UpgradeHelper extends DaoMaster.OpenHelper {
+
+    public static String TAG = UpgradeHelper.class.getSimpleName();
+
+    public UpgradeHelper(Context context, String name, SQLiteDatabase.CursorFactory factory) {
+        super(context, name, factory);
+    }
+
+    /**
+     * Here is where the calls to upgrade are executed
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        /* i represent the version where the user is now and the class named with this number implies that is upgrading from i to i++ schema */
+        for (int i = oldVersion; i < newVersion; i++) {
+            Log.i("greenDAO", "Upgrading schema from version " + oldVersion + " to " + newVersion + " by migrating all tables data");
+            MigrationHelper.getInstance().migrate(db, UserDao.class);
+        }
+    }
+}
+
